563877f2f8512afb81f51aa9ffc340b39c1b7d8a
anuragshetty1998 <shettyanurag9@gmail.com>
2022-08-28 22:43:20 +0530

    completed till group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { AddGroupComponent } from './components/add-group/add-group.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { MatIconModule } from '@angular/material/icon';
     GroupsComponent,
     ContactDetailsComponent,
     SearchComponent,
+    AddGroupComponent,
   ],
   imports: [
     BrowserModule,
